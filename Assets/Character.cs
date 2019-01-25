73b0736f937e129283e029c287913f2fb4be8017
blackBoyCode <e96e6e6@gmail.com>
2019-01-25 17:59:12 -0500

    jumping added

---
+++ Assets/Character.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    public Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (Input.GetKeyDown (KeyCode.Space))
+        {
+
+            rb.AddForce(new Vector3(0, 10, 0), ForceMode.Impulse);
+        }
+    }
+}
