d521df620e21273f48ee0b8a389841bdfec068e7
Jessica St-Pierre <jessica.stpierre@hotmail.com>
2019-02-18 21:45:17 -0500

    Fixed object sizes

---
+++ Assets/Scripts/GrowObject1.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrowObject1 : MonoBehaviour
+{
+    public float GrowLimitTest;
+   // float test;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("Grow", 0.5f, 0.01f);
+       // test = 0.1f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    void Grow() {
+
+        // test += 0.1f;
+
+        Vector3 test = new Vector3(0.5f, 0.5f, 0.5f);
+        if (transform.localScale.x != GrowLimitTest)
+            transform.localScale = transform.localScale + test;
+        else CancelInvoke();
+
+
+    }
+}
